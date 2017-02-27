4d392338c392a4900d27a10e89cf5d05c2b31f01
Andrew M. White <andreww@netflix.com>
2017-02-27 12:49:53 -0800

    Fix copyright declaration for Sphinx docs.

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Stethoscope'
-copyright = u'2017, Andrew M. White'
+copyright = u'2017, Netflix, Inc.'
 author = u'Andrew M. White'
 
 # The version info for the project you're documenting, acts as replacement for
